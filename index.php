c5b71f4e07d32a0a704f2470f555c04e2b062bd7
DaliBel <dalila.belkebir@labom2iformation.fr>
2023-07-25 13:57:54 +0200

    ajout du html et css - ajout 4 produits

--- index.php
+++ index.php
@@ -1,11 +1,36 @@
 <?php
 // Connexion à la base de données literie3000
-$dsn = "mysql:host=localhost;dbname=literie300";
+$dsn = "mysql:host=localhost;dbname=literie3000";
 $db = new PDO($dsn, "root", "");
 
 // Récupérer les produits de la table matelas
-$query = $db->query("SELECT id, name, marque, taille, largeur, longueur, matiere image, prix FROM matelas");
+$query = $db->query("SELECT id, name, marque, taille, largeur, longueur, matiere, picture, prix FROM matelas");
 // Récupérer les résultats au format tableau associatif
 $matelas = $query->fetchAll(PDO::FETCH_ASSOC);
 
+// Inclure le header
+include("templates/header.php");
+
 ?>
+<h1>Nos dernières nouveautés</h1>
+
+
+<div class="matelas">
+
+<?php
+foreach ($matelas as $mat) {
+?>
+<div class="mat">
+    <img src="img/matelas/<?= $mat["picture"] ?>" alt="">
+    <h2>
+        <?= $mat["name"] ?>
+    </h2>
+    <p>Taille : <?= $mat["taille"] ?></p>
+    <span>Prix : <?= $mat["prix"] ?></span>
+</div>
+<?php
+}
+
+?>
+
+</div>
--- literie3000.sql
+++ literie3000.sql
@@ -10,15 +10,15 @@ CREATE TABLE matelas (
     largeur SMALLINT,
     longueur SMALLINT,
     matiere VARCHAR(100),
-    image VARCHAR(255),
+    picture VARCHAR(255),
     prix SMALLINT
 );
 
 INSERT INTO matelas
-(name, marque, taille, largeur, longueur, matiere, image, prix)
+(name, marque, taille, largeur, longueur, matiere, picture, prix)
 VALUES
-("Pas touché", "Epeda", "1P", 90, 190, "ressorts", "https://www.lematelas.fr/media/catalog/product/cache/58d42f0437f84cb6a87786e02b22b5a3/m/a/matelas_epeda_ressorts_multi_air_poudre_fond_blanc.webp", 529),
-("Lapin", "Dreamway", "1P", 90, 190, "latex", "https://www.lematelas.fr/media/catalog/product/cache/58d42f0437f84cb6a87786e02b22b5a3/n/o/noe_ambiance_1.webp", 709),
-("Alejandrinho", "Bultex", "2P", 140, 190, "ressorts", "https://www.lematelas.fr/media/catalog/product/cache/58d42f0437f84cb6a87786e02b22b5a3/p/u/pureness.webp", 529),
-("Papy", "Epeda", "2P", 160, 190, "Mousse", "https://www.lematelas.fr/media/catalog/product/cache/58d42f0437f84cb6a87786e02b22b5a3/s/o/sommier_d_co_gris_universel_15cm_pieds_inclus_presente_avec_matelas__1.webp", 509),
+("Pas touché", "Epeda", "1P", 90, 190, "ressorts", "matelas_epeda_pastouche.jpg", 529),
+("Lapin", "Dreamway", "1P", 90, 190, "latex", "matelas_dreamway_lapin.jpg", 709),
+("Alejandrinho", "Bultex", "2P", 140, 190, "ressorts", "matelas_bultex_alejandrinho.jpg", 529),
+("Papy", "Epeda", "2P", 160, 190, "Mousse", "matelas_epeda_papy.jpg", 509);
 
